5603c959958052eb90c26d5ce921782431b07026
Sangmin Ahn <gijigae@gmail.com>
2022-04-14 06:50:11 +0900

    Initial commit

---
+++ Topics/Comparisons/Focus on the positive/main.py
@@ -0,0 +1,8 @@
+# Please don't modify this code
+# `a` stores an input value
+i = 0
+while i <= 10:
+    i = i + 1
+    if i > 7:
+        i = i + 2
+print(i)
---
+++ Topics/List/List from string/main.py
@@ -0,0 +1,7 @@
+# the following line reads the string to work with from the input, do not modify it, please
+input_str = input()
+
+# your code here
+
+tmp = [char for char in input_str]
+print(tmp)
